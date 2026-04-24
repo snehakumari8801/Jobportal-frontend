e4d78258590fe7fa07eaec838b35379701923917
sneha kumari <snehakumari8801@gmail.com>
2026-04-24 17:52:16 +0530

    add url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -38,7 +38,7 @@
 import axios from "axios";
 
 // ✅ Base URL (for socket, files, etc.)
-export const base = "https://jobportal-backend-111.onrender.com";
+export const base = "https://jobportal-backend-1234.onrender.com";
 // export const base = "http://localhost:5000";
 
 
